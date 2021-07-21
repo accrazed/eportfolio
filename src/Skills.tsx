07857d114366a937019b0d7c715c4f774e4621f8
AccraZed <acastilla@vmware.com>
2021-07-21 17:57:37 -0400

    create framework for skills logos

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -1,17 +1,42 @@
+import logoGo from './assets/logo_go.svg';
+import logoRust from './assets/logo_rust.svg';
+
 type Skill = {
     name: string;
-    img: URL;
-    example?: string;
+    img: string;
+    example?: URL;
 };
 
 const skills: Skill[] = [
     {
         name: 'Golang',
-        img: new URL('file:./assets/logo_go.svg'),
+        img: logoGo,
+        example: new URL('https://github.com/AccraZed/node-run-script'),
+    },
+    {
+        name: 'Rust',
+        img: logoRust,
+        example: new URL(
+            'https://github.com/AccraZed/language-reinforcement/tree/master/rust-projects/maze'
+        ),
     },
 ];
 const Skills: Function = (): JSX.Element => {
-    return <img src={skills[0].img.toString()} />;
+    let logos = skills.map((skill: Skill, i: number) => {
+        let svg = (
+            <img
+                key={skill.name + i}
+                src={skill.img}
+                alt={skill.name + ' logo'}
+                className={'logo-' + skill.name}
+            />
+        );
+        if (skill.example !== undefined) {
+            return <a href={skill.example.toString()}>{svg}</a>;
+        }
+        return svg;
+    });
+    return <div className="logo-grid">{logos}</div>;
 };
 
 export default Skills;
